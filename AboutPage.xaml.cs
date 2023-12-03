7ca7cb2ec2ce9ffa41f114abb00be5f6cdaca936
ALEXANDRA-MARIA GREAB <alexandra.greab@stud.ubbcluj.ro>
2023-12-03 19:25:49 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Greab_Alexandra_lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Greab_Alexandra_lab7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
